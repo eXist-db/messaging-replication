a9bbbfd283ba0c9b83963ed883365327c16aee49
Dannes Wessels <dannes@exist-db.org>
2013-09-23 21:12:50 +0200

    Make the listener actually call the xquery function. Added JMS-config-map. Code does not work yet…..

--- java/src/org/exist/messaging/xquery/Receiver.java
+++ java/src/org/exist/messaging/xquery/Receiver.java
@@ -18,8 +18,10 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import org.apache.log4j.Logger;
 import org.exist.messaging.configuration.JmsConfiguration;
+import org.exist.xquery.XPathException;
 import org.exist.xquery.XQueryContext;
 import org.exist.xquery.functions.map.MapType;
+import org.exist.xquery.value.AtomicValue;
 import org.exist.xquery.value.BooleanValue;
 import org.exist.xquery.value.DecimalValue;
 import org.exist.xquery.value.DoubleValue;
@@ -90,8 +92,8 @@ public class Receiver {
 
     private static class MyJMSListener implements MessageListener {
 
-        private FunctionReference ref;
-        private XQueryContext context;
+        private FunctionReference functionReference;
+        private XQueryContext xqueryContext;
 
 //        public MyJMSListener() {
 //        }
@@ -101,7 +103,7 @@ public class Receiver {
                 LOG.info(String.format("msgId='%s'", msg.getJMSMessageID()));
 
                 // Show content
-                Item content = null;
+                AtomicValue content = null;
                 
                 if (msg instanceof TextMessage) {
                     LOG.info("TextMessage");
@@ -137,44 +139,73 @@ public class Receiver {
                 LOG.info(String.format("content='%s' type='%s'", content, msg.getJMSType()));
                 
                 // Copy property values into Maptype
-                MapType map = new MapType(context);
-              
-                Enumeration props = msg.getPropertyNames();
-                while (props.hasMoreElements()) {
-                    String elt = (String) props.nextElement();
-                    String value = msg.getStringProperty(elt);
-                    LOG.info(String.format("'%s'='%s'", elt, value));
-                    map.add( new StringValue(elt), new ValueSequence(new StringValue(value)));
-                }
+                MapType msgProperties = getMessageProperties(msg, xqueryContext);
+                MapType jmsProperties = getJmsProperties(msg, xqueryContext);
                 
                 // Setup parameters callback function
-                Sequence params[] = new Sequence[2];
-                params[0] = new ValueSequence(content);
-                params[1] = map;
+                Sequence params[] = new Sequence[3];
+                params[0] = content;
+                params[1] = msgProperties;
+                params[2] = jmsProperties;
                 
                 // Execute callback function
-                ref.evalFunction(null, null, params);
+                functionReference.evalFunction(null, null, params);
 
             } catch (JMSException ex) {
                 LOG.error(ex);
+                ex.printStackTrace();
 
-//            } catch (XPathException ex) {
-//                LOG.error(ex);
+            } catch (XPathException ex) {
+                LOG.error(ex);
+                ex.printStackTrace();
 
             } catch (Throwable ex) {
                 LOG.error(ex);
+                ex.printStackTrace();
             }
 
         }
 
         public void setFunctionReference(FunctionReference ref) {
-            this.ref = ref;
+            this.functionReference = ref;
         }
         
         public void setXQueryContext(XQueryContext context) {
-            this.context = context;
+            this.xqueryContext = context;
         }
         
+        private MapType getMessageProperties(Message msg, XQueryContext xqueryContext) throws XPathException, JMSException {
+            // Copy property values into Maptype
+            MapType map = new MapType(xqueryContext);
+
+            Enumeration props = msg.getPropertyNames();
+            while (props.hasMoreElements()) {
+                String elt = (String) props.nextElement();
+                String value = msg.getStringProperty(elt);
+                add(map, elt, value);
+            }
+            return map;
+        }
+        
+        private MapType getJmsProperties(Message msg, XQueryContext xqueryContext) throws XPathException, JMSException {
+            // Copy property values into Maptype
+            MapType map = new MapType(xqueryContext);
+
+            add(map, "JMSMessageID", msg.getJMSMessageID());
+            add(map, "JMSCorrelationID", msg.getJMSCorrelationID());
+            add(map, "JMSType", msg.getJMSType());
+            add(map, "JMSPriority", ""+msg.getJMSPriority());
+            add(map, "JMSExpiration", ""+msg.getJMSExpiration());
+            add(map, "JMSTimestamp", ""+msg.getJMSTimestamp());
+            
+            return map;
+        }
+        
+        private void add(MapType map, String key, String value) throws XPathException {
+            if (map != null && key != null && !key.isEmpty() && value != null) {
+                map.add(new StringValue(key), new ValueSequence(new StringValue(value)));
+            }
+        }
         
     }
 }
