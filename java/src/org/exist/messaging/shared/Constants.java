356c8dff665e6927e59a83d47bb91ae4dad7507d
Dannes Wessels <dannes@exist-db.org>
2013-10-11 12:28:08 +0200

    added some comments

--- java/src/org/exist/messaging/shared/Constants.java
+++ java/src/org/exist/messaging/shared/Constants.java
@@ -26,33 +26,50 @@ package org.exist.messaging.shared;
  */
 public class Constants {
 
+    /*
+     * eXist-db knows about XML and NON-XML data
+     */
     public static final String DATA_TYPE_BINARY = "binary";
     public static final String DATA_TYPE_XML = "xml";
-    
+    /*
+     * eXist-db internal nata
+     */
     public static final String EXIST_DATA_TYPE = "exist.data.type";
     public static final String EXIST_DOCUMENT_MIMETYPE = "exist.document.mimetype";
     public static final String EXIST_DOCUMENT_URI = "exist.document.uri";
     public static final String EXIST_XPATH_DATATYPE = "exist.xpath.datatype";
-    
-    public static final String EXIST_INSTANCE_ID = "exist.instance.id";
-    
-    public static final String JMS = "JMS";
-    public static final String JMS_CORRELATION_ID = "JMSCorrelationID";
-    public static final String JMS_EXPIRATION = "JMSExpiration";
-    public static final String JMS_MESSAGE_ID = "JMSMessageID";
-    public static final String JMS_PRIORITY = "JMSPriority";
-    public static final String JMS_TIMESTAMP = "JMSTimestamp";
-    public static final String JMS_TYPE = "JMSType";
-    
+    /*
+     * eXist-db JMS instance id
+     */
+    public static final String EXIST_INSTANCE_ID = "exist.instance-id";
+    /*
+     * JMS reporting
+     */
+    public static final String JMS = "jms";
+    public static final String JMS_CORRELATION_ID = "jms.correlation-id";
+    public static final String JMS_EXPIRATION = "jms.expiration";
+    public static final String JMS_MESSAGE_ID = "jms.message-id";
+    public static final String JMS_PRIORITY = "jms.priority";
+    public static final String JMS_TIMESTAMP = "jms.timestamp";
+    public static final String JMS_TYPE = "jms.type";
+    /*
+     * Parameter names for setting-up a connection
+     */
     public static final String CONNECTION_FACTORY = "connection-factory";
     public static final String DESTINATION = "destination";
-    
+    /*
+     * Connection parameters
+     */
     public static final String JMS_CONNECTION_USERNAME = "connection.username";
     public static final String JMS_CONNECTION_PASSWORD = "connection.password";
     public static final String CLIENT_ID = "connection.client-id";
-    
+    /**
+     * JMS message selector
+     */
     public static final String MESSAGE_SELECTOR = "consumer.message-selector";
-    
+    /*
+     * Topic parameters (durable, prefent backfire)
+     */
     public static final String DURABLE = "subscriber.durable";
     public static final String NO_LOCAL = "subscriber.nolocal";
     public static final String SUBSCRIBER_NAME = "subscriber.name";
