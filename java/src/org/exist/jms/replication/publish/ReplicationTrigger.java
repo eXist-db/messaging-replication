eef51eb0c7bf9bcfd29e55324ba9de9ae4c3bc9c
Dannes Wessels <dannes@exist-db.org>
2016-10-01 16:34:12 +0200

    Remove old code to check exist-2-2+, not needed anymore

--- java/src/org/exist/jms/replication/publish/ReplicationTrigger.java
+++ java/src/org/exist/jms/replication/publish/ReplicationTrigger.java
@@ -53,42 +53,17 @@ public class ReplicationTrigger extends SAXTrigger implements DocumentTrigger, C
     private static final String BLOCKED_MESSAGE = "Prevented re-replication of '%s'";
     private Map<String, List<?>> parameters;
 
-    private boolean isOriginIdAvailable = false;
-
-    /**
-     * Constructor.
-     * <p>
-     * Verifies if new-enough version of eXist-db is used.
-     */
-    public ReplicationTrigger() {
-
-        super();
-
-        try {
-            // Verify if method does exist
-            Class.forName("org.exist.Transaction");
-
-            // Yes :-)
-            isOriginIdAvailable = true;
-
-        } catch (final java.lang.ClassNotFoundException error) {
-
-            // Running an old version of eXist-db
-            LOGGER.info("Method Txn.getOriginId() is not available. Please upgrade to eXist-db 2.2 or newer. " + error.getMessage());
-        }
-
-    }
 
     /**
      * Verify if the transaction is started by the JMX extension
      *
      * @param transaction The original transaction
-     * @return TRUE when started from JMS else FALSE.
+     * @return TRUE when started from the eXist-db JMS else FALSE.
      */
     private boolean isJMSOrigin(final Txn transaction) {
 
-        // only try to get OriginId when metjod is available.
-        final String originId = isOriginIdAvailable ? transaction.getOriginId() : null;
+        // Get originId.
+        final String originId = transaction.getOriginId();
 
         return StringUtils.startsWith(originId, JMS_EXTENSION_PKG);
     }
@@ -103,7 +78,7 @@ public class ReplicationTrigger extends SAXTrigger implements DocumentTrigger, C
             LOGGER.debug(document.getURI().toString());
         }
 
-        /** TODO: make optional? (for lJO) */
+        /* TODO: make optional? (for lJO) */
         if (isJMSOrigin(transaction)) {
             LOGGER.info(String.format(BLOCKED_MESSAGE, document.getURI().toString()));
             return;
@@ -267,12 +242,12 @@ public class ReplicationTrigger extends SAXTrigger implements DocumentTrigger, C
     }
 
     //@Override
-    public void beforeUpdateCollectionMetadata(DBBroker broker, Txn txn, Collection collection) throws TriggerException {
+    public void beforeUpdateCollectionMetadata(final DBBroker broker, final Txn txn, final Collection collection) throws TriggerException {
         // ignored
     }
 
     //@Override
-    public void afterUpdateCollectionMetadata(DBBroker broker, Txn txn, Collection collection) throws TriggerException {
+    public void afterUpdateCollectionMetadata(final DBBroker broker, final Txn txn, final Collection collection) throws TriggerException {
         if (LOGGER.isDebugEnabled()) {
             LOGGER.debug(collection.getURI().toString());
         }
